de07a3bbedfe982a5879a717187fae15b1ca43a0
Skerdian Shehaj <skerdianshehaj@gmail.com>
2023-09-25 00:45:41 +0200

    Created StarRating component and its API (config props)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+/* import "./index.css";
+import App from "./App"; */
+import StarRating from "./components/StarRating";
+
+const Test = () => {
+  const [rate, setRate] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        maxRating={5}
+        onSetRating={setRate}
+        defaultRating={3}
+        messages={["Terrible!", "Bad!", "Okay!", "Good!", "Amazing!"]}
+        color="#FF0000"
+        size={5}
+      />
+      <p>You rated {rate}</p>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>
 );
